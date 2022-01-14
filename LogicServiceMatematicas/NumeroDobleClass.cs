09efb296b3b824f153b2a43b21708994fec9a87b
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-14 16:17:30 +0100

    commit

---
+++ LogicServiceMatematicas/NumeroDobleClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicServiceMatematicas
+{
+    public class NumeroDobleClass : INumeroDobleContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Bienvenido a Servicios WCF, " + nombre;
+        }
+
+        public string MetodoInvisible()
+        {
+            return "Ni lo vamos a ver...";
+        }
+    }
+}

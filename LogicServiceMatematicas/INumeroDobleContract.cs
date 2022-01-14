350dfee0af490647e87840fce6ee5352d2c242ac
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-14 16:10:32 +0100

    fff

---
+++ LogicServiceMatematicas/INumeroDobleContract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicServiceMatematicas
+{
+    [ServiceContract]
+    public interface INumeroDobleContract
+    {
+        //SOLAMENTE DECLARAMOS METODOS
+        int GetNumeroDoble(int numero);
+        string GetSaludo(string nombre);
+        string MetodoInvisible();
+    }
+}

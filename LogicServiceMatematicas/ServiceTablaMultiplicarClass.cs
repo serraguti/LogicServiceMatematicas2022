0c46eb27bea5f50bd02db263fb770592cc1fc535
serraguti <46522403+serraguti@users.noreply.github.com>
2022-01-14 17:21:30 +0100

    commit

---
+++ LogicServiceMatematicas/ServiceTablaMultiplicarClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicServiceMatematicas
+{
+    public class ServiceTablaMultiplicarClass : IServiceTablaMultiplicar
+    {
+        public List<TablaMultiplicar> GetTablaMultiplicar(int numero)
+        {
+            List<TablaMultiplicar> lista = new List<TablaMultiplicar>();
+            for (int i = 1; i <= 10; i++)
+            {
+                int resultado = numero * i;
+                string operacion = numero + " * " + i;
+                TablaMultiplicar tabla = new TablaMultiplicar();
+                tabla.Operacion = operacion;
+                tabla.Resultado = resultado;
+                lista.Add(tabla);
+            }
+            return lista;
+        }
+    }
+}
---
+++ LogicServiceMatematicas/TablaMultiplicar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicServiceMatematicas
+{
+    [DataContract]
+    public class TablaMultiplicar
+    {
+        [DataMember]
+        public String Operacion { get; set; }
+        [DataMember]
+        public int Resultado { get; set; }
+    }
+}
